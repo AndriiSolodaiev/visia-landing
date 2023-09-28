1d0260ee5ab1991f8931165df2b1a646b49f2975
AndriiSolodaiev <andik05016@gmail.com>
2023-09-29 02:37:31 +0300

    animate all site

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -1,5 +1,7 @@
 import Swiper from 'swiper';
+import { Scrollbar } from 'swiper';
 import 'swiper/css';
+import 'swiper/css/scrollbar';
 import '../modules/effects/imgParallax';
 import { gsap, ScrollTrigger } from 'gsap/all';
 import Lenis from '@studio-freight/lenis';
@@ -19,10 +21,15 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 const swiper = new Swiper('.swiper', {
+  modules: [Scrollbar],
   speed: 1000,
   spaceBetween: 50,
   // Navigation arrows
   slidesPerView: 1,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
   breakpoints: {
     1366: {
       slidesPerView: 2,
@@ -80,7 +87,6 @@ function animateText(selector) {
         trigger: selector,
         start: 'top+=200px center',
         end: '+=10%',
-        markers: true,
       },
       onComplete: () => {
         if (index === textCharacters.length - 1) {
@@ -101,7 +107,6 @@ const aboutTl = gsap.timeline({
     trigger: '.about',
     start: 'top+=20% center',
     end: '+=10%',
-    markers: true,
   },
 });
 aboutTl
@@ -120,7 +125,7 @@ const aboutImgTl = gsap.timeline({
     trigger: '.about__img-wrap',
     start: 'top center',
     end: '+=50%',
-    markers: true,
+
     scrub: 1,
   },
 });
@@ -144,7 +149,6 @@ const missionTl = gsap.timeline({
     trigger: '.mission',
     start: 'top+=20% center',
     end: '+=10%',
-    markers: true,
   },
 });
 
@@ -165,7 +169,6 @@ const teamTl = gsap.timeline({
     trigger: '.team',
     start: 'top center',
     end: '+=10%',
-    markers: true,
   },
 });
 
@@ -173,6 +176,128 @@ teamTl
   .from('.team .section-title', { scale: 0, duration: 0.3 })
   .add(animateText('.team'), '<0.2')
   .from('.mission__descr-container p', { yPercent: -100, opacity: 0, duration: 1.2 }, '<0.5')
-  .from('.founders-card', { xPercent: 100, autoAlpha: 0, stagger: 0.3, duration: 2 }, '<');
+  .fromTo(
+    '.founders-card',
+    { xPercent: 100, autoAlpha: 0, stagger: 0.3, duration: 2 },
+    { xPercent: 0, autoAlpha: 1 },
+    '<',
+  );
 
-// gsap.timeline({})
+// const teamMembersTl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: '.team',
+//     start: 'top center',
+//     end: '+=10%',
+//
+//   },
+// });
+document.querySelectorAll('.member-card').forEach((member, index, array) => {
+  // if (index < 4) {
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: array[index],
+        start: 'top 70%',
+        end: '+=50%',
+      },
+    })
+    .fromTo(
+      member,
+      {
+        opacity: 0,
+        y: (100 + Math.random() * -200) * 2,
+        delay: 0.1 * Math.random() * 4,
+        rotateY: -45,
+        rotateX: 15,
+        rotateY: 45,
+        clipPath: 'polygon(0 0, 0 100%, 100% 0, 0% 100%)',
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 0.1 * Math.random() * 4,
+        rotateY: 0,
+        rotateX: 0,
+        rotateY: 0,
+        clipPath: 'polygon(0 0, 100% 0%, 100% 100%, 0% 100%)',
+        duration: 0.7,
+      },
+    );
+  // }
+  // if (4 <= index && index < 7) {
+  //   gsap
+  //     .timeline({
+  //       scrollTrigger: {
+  //         trigger: array[5],
+  //         start: 'top 80%',
+  //         end: '+=50%',
+  //       },
+  //     })
+  //     .from(member, { opacity: 0, y: -100, delay: index * 0.2 });
+  // }
+  // if (7 <= index && index < 10) {
+  //   gsap
+  //     .timeline({
+  //       scrollTrigger: {
+  //         trigger: array[8],
+  //         start: 'top 80%',
+  //         end: '+=50%',
+  //       },
+  //     })
+  //     .from(member, { opacity: 0, x: 100, delay: Math.pow(0.2, index - 7) });
+  // }
+  // if (10 <= index && index < 12) {
+  //   gsap
+  //     .timeline({
+  //       scrollTrigger: {
+  //         trigger: array[10],
+  //         start: 'top 80%',
+  //         end: '+=50%',
+  //       },
+  //     })
+  //     .from(member, { opacity: 0, y: 100, delay: index * 0.01 });
+  // }
+  // return;
+});
+const fillerTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.filler',
+    start: 'top+=10% center',
+    end: '+=50%',
+  },
+});
+fillerTl
+  .from('.filler-title h2', {
+    autoAlpha: 0,
+    xPercent: -50,
+    duration: 1.2,
+  })
+  .from(
+    '.filler__second-title',
+    {
+      autoAlpha: 0,
+      xPercent: 50,
+      duration: 1.2,
+    },
+    '<',
+  );
+
+const projectsTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.projects',
+    start: 'top+=20% center',
+    end: '+=10%',
+  },
+});
+projectsTl
+  .from('.projects .section-title', { scale: 0, duration: 0.3 })
+  .add(animateText('.projects'), '<0.2')
+  .from('.projects__descr-container p', { yPercent: -100, opacity: 0, duration: 1.2 }, '<0.5')
+  .from(
+    '.projects__advantages-wrap p',
+    { autoAlpha: 0, duration: 1.2, stagger: 0.2 },
+
+    '<0.5',
+  )
+  .to('.swiper-wrapper', { x: -100 }, '<')
+  .to('.swiper-wrapper', { x: 0 });
